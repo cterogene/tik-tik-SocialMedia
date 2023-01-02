255d1ab7991cf8aed01289be5d492d6fbd5f186c
Terogene Claudio <cterogene@gmail.com>
2023-01-02 09:40:12 -0500

    basic functionaly implemented and setup ready to build Tik Tik in next js

--- tik-tik/pages/_app.tsx
+++ tik-tik/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
+ const App =({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState (true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if(isSSR) return null;
+
   return <Component {...pageProps} />
 }
+export default App
