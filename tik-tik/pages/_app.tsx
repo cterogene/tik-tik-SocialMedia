c3903acfa7edad8be4d9338d95859e61f2705f96
Terogene Claudio <cterogene@gmail.com>
2023-01-03 20:06:48 -0500

    added Navbar and Sidebar basic template

--- tik-tik/pages/_app.tsx
+++ tik-tik/pages/_app.tsx
@@ -3,15 +3,26 @@ import type { AppProps } from 'next/app'
 import { useState } from 'react'
 import { useEffect } from 'react'
 
- const App =({ Component, pageProps }: AppProps) => {
-  const [isSSR, setIsSSR] = useState (true);
+const App = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
 
   useEffect(() => {
     setIsSSR(false);
   }, []);
 
-  if(isSSR) return null;
+  if (isSSR) return null;
 
-  return <Component {...pageProps} />
+  return (
+    <div>
+      Navbar 
+      <div className='flex gap-6 md: gat-20'>
+        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+            Sidebar
+        </div>
+
+      </div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 export default App
--- tik-tik/pages/index.tsx
+++ tik-tik/pages/index.tsx
@@ -1,4 +1,4 @@
-
+import type { NextPage } from "next"
 
 export default function Home() {
   return (
