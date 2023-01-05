09e637154eca2f854e84ba732fa6061a4d59433f
Terogene Claudio <cterogene@gmail.com>
2023-01-05 08:20:55 -0500

    adde the utils files with the tiktik logo and files for the project

--- tik-tik/components/Navbar.tsx
+++ tik-tik/components/Navbar.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import Logo from '../utils/tiktik-logo.png';
 const Navbar = () => {
-  return (
-    <div>
-    Navbar
-    </div>
-  )
+    return (
+        <div className='w-full flex justify-between items-center boder-b-2 
+        border-gray-200 py-2 px-4'>
+            <Link href='/'>
+                <div>
+                    <Image className='cursor-pointer'
+                        src={Logo}
+                        alt="TikTik"
+                        layout='responsive' />
+
+                </div>
+            </Link>
+        </div>
+    )
 }
 
 export default Navbar
