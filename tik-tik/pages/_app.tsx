276d94b9e035189dec34786f1d860fa4c22f0001
Terogene Claudio <cterogene@gmail.com>
2023-01-05 07:57:35 -0500

    created the Navbar and Sidebar components

--- tik-tik/pages/_app.tsx
+++ tik-tik/pages/_app.tsx
@@ -2,6 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
+
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -14,14 +18,17 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <div>
-      Navbar 
-      <div className='flex gap-6 md: gat-20'>
+      <Navbar /> 
+      <div className='flex gap-6 md:gap-20'>
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-            Sidebar
+            <Sidebar />
+        </div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+        <Component {...pageProps} />
         </div>
 
       </div>
-      <Component {...pageProps} />
+      
     </div>
   );
 }
