2ba19e9b83dfd8da6094401bb80897911a919ff7
Terogene Claudio <cterogene@gmail.com>
2023-01-05 08:25:57 -0500

    basic navbar implemented with the Tiktik Logo

--- tik-tik/components/Navbar.tsx
+++ tik-tik/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <div className='w-full flex justify-between items-center boder-b-2 
         border-gray-200 py-2 px-4'>
             <Link href='/'>
-                <div>
+                <div className='w-[100px] md:w-[130px]'>
                     <Image className='cursor-pointer'
                         src={Logo}
                         alt="TikTik"
