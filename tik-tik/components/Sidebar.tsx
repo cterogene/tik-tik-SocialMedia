d6679d1aef9e950d2156e47557224b00a728aa24
Terogene Claudio <cterogene@gmail.com>
2023-01-05 20:48:34 -0500

    Sidebar medium menu implemented with menu and cancel icons

--- tik-tik/components/Sidebar.tsx
+++ tik-tik/components/Sidebar.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import {AiFillHome, AiOutlineMenu} from 'react-icons/ai';
+import {ImCancelCircle} from 'react-icons/im';
 const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
   return (
     <div>
-      Sidebar
+     <div className='block xl:hidden m-2 ml-4 mt-3 text-xl' onClick={() => setShowSidebar((prev) => !prev )}>
+      {showSidebar ?   <AiOutlineMenu />: <ImCancelCircle /> }
+
+     </div>
     </div>
   )
 }
