eee5598b9301431c95cf335cddcaf93937620075
Terogene Claudio <cterogene@gmail.com>
2023-01-11 19:17:38 -0500

    Customized Discover components and edited constant topics. Sidebar width minimized

--- tik-tik/components/Sidebar.tsx
+++ tik-tik/components/Sidebar.tsx
@@ -19,7 +19,7 @@ const Sidebar = () => {
         {showSidebar ? <AiOutlineMenu /> : <ImCancelCircle />}
       </div>
       {showSidebar && (
-        <div className='xl:w-400 w-30 flex flex-col justify-start mb-10
+        <div className='xl:w-400 w-20 flex flex-col justify-start mb-10
       border-r-2 border-gray-100 xl:border-0 p-3'>
           <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
             <Link href="/">
