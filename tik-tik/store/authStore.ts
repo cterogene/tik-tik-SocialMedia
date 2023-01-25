6c101e56d1481f1f0421812a1f1bbec969b4e0e7
Terogene Claudio <cterogene@gmail.com>
2023-01-24 20:04:02 -0500

    zustand base setup for storing user on local storage

--- tik-tik/store/authStore.ts
+++ tik-tik/store/authStore.ts
@@ -1,1 +1,18 @@
 import create from 'zustand';
+import {persist} from 'zustand/middleware';
+import axios from 'axios';
+import { GiNuclear } from 'react-icons/gi';
+
+const authStore = (set: any) => ({
+userProfile: null,
+addUser: (user:any) => set({userProfile: user})
+
+});
+
+const useAuthStore = create(
+    persist(authStore, {
+        name: 'auth'
+    })
+)
+
+export default useAuthStore;
--- tik-tik/utils/index.ts
+++ tik-tik/utils/index.ts
@@ -3,7 +3,7 @@ import jwt_decode from 'jwt-decode';
 
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const createOrGetUser = async (response: any) => {
+export const createOrGetUser = async (response: any, addUser: any) => {
    const decoded: {name: string, picture: string, sub: string} = jwt_decode(response.credential);
   
   
