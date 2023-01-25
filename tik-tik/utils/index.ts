d857cff787589ab6775575f4cb60dc19488528fc
Terogene Claudio <cterogene@gmail.com>
2023-01-24 20:46:27 -0500

    profile set and login completed

--- tik-tik/utils/index.ts
+++ tik-tik/utils/index.ts
@@ -16,6 +16,7 @@ export const createOrGetUser = async (response: any, addUser: any) => {
     image: picture,
   };
   
+  addUser(user);
 
   await axios.post(`http://localhost:3000/api/auth`, user);
 };
